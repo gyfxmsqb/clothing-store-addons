725d6a4e512865bfad4241c3dfca938247e65bae
Bogo Tech <bogotechnology@gmail.com>
2020-04-27 15:23:49 -0700

    added the header and links to home, shop, and contact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { HomePage } from './pages/homepage/homepage';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header';
 
 
 const HatsPage = () => (
@@ -12,9 +13,12 @@ const HatsPage = () => (
 
 function App() {
   return (
+
     <div >
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage} />
+
         <Route exact path="/hats" component={HatsPage} />
         <Route exact path="/shop" component={ShopPage} />
       </Switch>
