36114faef1f5a40414cc4c411205a14c29dfa1e5
Bogo Tech <bogotechnology@gmail.com>
2020-04-28 14:25:58 -0700

    Finished signup and signin with email and password

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import "./sign-in.scss"
 import FormInput from '../form-input/form-input'
 import { CustomButton } from '../custom-button/custom-button'
-import { signInWithGoogle } from '../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 
 class SignIn extends Component {
 
@@ -16,9 +16,19 @@ class SignIn extends Component {
         }
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async event => {
         event.preventDefault()
         this.setState({ email: "", password: "" })
+
+        const {email, password} = this.state
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({email:"", password:""})
+        } catch (error) {
+            alert(error.message);
+            
+        }
 
     }
 
