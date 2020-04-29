d7698a5301245e306cbfc384f985847c0fecebbd
Bogo Tech <bogotechnology@gmail.com>
2020-04-29 15:40:21 -0700

    Finished the cartDropdown menu with the display of the itmes and quantities

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -1,17 +1,40 @@
 import React from 'react'
 import "./cart-dropdown.scss"
 import { CustomButton } from '../custom-button/custom-button'
+import CartItem from '../cart-item/cart-item'
+import { connect } from 'react-redux'
 
 
-const CartDropdown = () => (
-    <div className = "cart-dropdown">
+const CartDropdown = ({cartItems}) => (
+    <div className="cart-dropdown">
 
         <div className="cart-items">
 
+            {
+                cartItems.map(cartItem=> (
+                    <CartItem key = {cartItem.id} item = {cartItem}/>
 
-            <CustomButton>GO TO CHECKOUT</CustomButton>
+                ))
+
+
+            }
 
         </div>
+            <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
-export default CartDropdown
+
+
+// //here is to fetch the data from redux 
+// const mapStateToProps = ({
+//     //cart from root-reducer cartItems from cartReducer second carItems are the props 
+//     cart: { cartItems } }) => ({
+//         cartItems
+//     })
+const mapStateToProps = state => ({
+    cartItems: state.cart.cartItems
+    })
+
+
+export default connect(mapStateToProps)(CartDropdown)
+
